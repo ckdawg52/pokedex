3db3cedc14d4e8adff3c653df7635fac5c9c6ed5
ckdawg52 <ckdawg5250@gmail.com>
2026-07-06 22:57:20 +0000

    Finished adding catch command and the inspect command

--- repl.go
+++ repl.go
@@ -91,5 +91,10 @@ func getCommands() map[string]cliCommand {
 			description:  "Attempt to catch a pokemon",
 			callback:     commandCatch,
 		},
+		"inspect": {
+			name:        "inspect <pokemon_name>",
+			description: "View details about a caught Pokemon",
+			callback:    commandInspect,
+		},
 	}
 }
